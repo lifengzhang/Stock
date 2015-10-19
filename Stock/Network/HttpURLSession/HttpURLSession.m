0a871f7d5fa8c9bd47c9f464561fb7ecbee3f2ea
michaelzlf <lifeng890510@foxmail.com>
2015-10-19 22:22:29 +0800

    add defaultSession for test

--- Stock/Network/HttpURLSession/HttpURLSession.m
+++ Stock/Network/HttpURLSession/HttpURLSession.m
@@ -10,4 +10,28 @@
 
 @implementation HttpURLSession
 
+-(instancetype)init {
+    self = [super init];
+    if (self) {
+        [self initDefaultSession];
+    }
+    return self;
+}
+
+- (void)initDefaultSession{
+    NSURLSessionConfiguration *defaultSessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    self.defaultSession = [NSURLSession sessionWithConfiguration:defaultSessionConfiguration delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    
+    [[self.defaultSession dataTaskWithURL:[NSURL URLWithString:@"http://ctxalgo.com/api/stocks"] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"Got response %@ with error %@.\n",response,error);
+        
+        NSError *Error;
+        NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableLeaves error:&Error];
+        
+        NSLog(@"DATA:\n%@\nEND DATA\n",jsonDict);
+
+    }]resume];
+}
+
 @end
